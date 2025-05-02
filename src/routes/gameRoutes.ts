fef9af368f16e117367e0f61a34958c3d49f19c0
natanrizzo <natan.rizzo22@gmail.com>
2025-05-01 22:14:24 -0300

    :sparkles: minor features

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -5,7 +5,17 @@ const router = Router();
 const rawgController = new RawgController();
 
 router.get("/", rawgController.fetchGames);
-router.get("/:id", rawgController.fetchGameById);
 router.get("/search", rawgController.searchGames);
+
+router.param("id", (req, res, next, id) => {
+    const num = Number(id);
+    if (!Number.isInteger(num)) {
+        return res.status(400).send("Invalid ID");
+    }
+    (req as any).gameId = num;
+    next();
+});
+
+router.get("/:id", rawgController.fetchGameById);
 
 export default router;
